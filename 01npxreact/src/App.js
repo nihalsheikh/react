a9db2c84ace217fc9c1140f6fa8bf98200a0aba2
nihalsheikh <nihalsheikh585@gmail.com>
2025-08-09 16:49:42 +0530

    Creating a component, React Fragment, Naming Convention and Extensions

--- 01npxreact/src/App.js
+++ 01npxreact/src/App.js
@@ -1,7 +1,14 @@
+import Chai from "./Chai"
+
+
 function App() {
   return (
-    <h1>Hello World: NPX React app</h1>
+    <>
+      <h1>Hello World: NPX React app</h1>
+      <Chai />
+    </>
   )
+
 }
 
 export default App;
