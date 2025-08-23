3b6a53ec35571c6e3c133e92ad3e5b1f085d0b4f
nihalsheikh <nihalsheikh585@gmail.com>
2025-08-23 09:43:08 +0530

    Context, Provider and useContext discussed

--- 08miniContext/src/App.jsx
+++ 08miniContext/src/App.jsx
@@ -1,4 +1,3 @@
-import Card from "./components/Card";
 import UserContextProvider from "./context/UserContextProvider";
 import Login from './components/Login';
 import Profile from "./components/Profile";
@@ -9,8 +8,6 @@ function App() {
 			<h1 className="text-4xl text-center bg-emerald-500 mt-4 p-4 font-bold">
 				Context API using .provider from react
 			</h1>
-
-			{/* <Card /> */}
 			<Login />
 			<Profile />
 		</UserContextProvider>
