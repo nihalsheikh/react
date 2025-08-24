3cac018af48eb5c24cf6b653e01146f42a5a9488
nihalsheikh <nihalsheikh585@gmail.com>
2025-08-24 09:47:11 +0530

    Redux started

---
+++ 11reduxToolkitTodo/src/App.jsx
@@ -0,0 +1,16 @@
+import "./App.css";
+
+function App() {
+	return (
+		<>
+			<h1 className="m-2 p-2 text-4xl font-bold text-white bg-emerald-700 text-center">
+				Redux: React-Redux and Redux Toolkit
+			</h1>
+			<h2 className="text-center m-2 p-2 text-3xl bg-fuchsia-700 text-white">
+				Todo App
+			</h2>
+		</>
+	);
+}
+
+export default App;
