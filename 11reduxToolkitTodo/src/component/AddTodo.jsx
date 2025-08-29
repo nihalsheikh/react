f9d4ce6809373522ceadf35aa0d890d73bbf2def
nihalsheikh <nihalsheikh585@gmail.com>
2025-08-29 12:07:22 +0530

    fixing things

--- 11reduxToolkitTodo/src/component/AddTodo.jsx
+++ 11reduxToolkitTodo/src/component/AddTodo.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import { useDispatch } from 'react-redux';
-import { removeTodo, addTodo } from "../features/todo/todoSlice"
+import { addTodo } from "../features/todo/todoSlice"
 
 function AddTodo() {
 	const [input, setInput] = useState("")
