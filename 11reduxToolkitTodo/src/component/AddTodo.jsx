f282012fd820f4ccb15eac150b379ffcf5eaadb3
nihalsheikh <nihalsheikh585@gmail.com>
2025-08-29 16:56:37 +0530

    tidiying up code

--- 11reduxToolkitTodo/src/component/AddTodo.jsx
+++ 11reduxToolkitTodo/src/component/AddTodo.jsx
@@ -1,20 +1,23 @@
 import React, { useState } from "react";
-import { useDispatch } from 'react-redux';
-import { addTodo } from "../features/todo/todoSlice"
+import { useDispatch } from "react-redux";
+import { addTodo } from "../features/todo/todoSlice";
 
 function AddTodo() {
-	const [input, setInput] = useState("")
+	const [input, setInput] = useState("");
 
-	const dispatch = useDispatch()
+	const dispatch = useDispatch();
 
 	const addTodoHandler = (e) => {
-		e.preventDefault()
-		dispatch(addTodo(input))
-		setInput('')
-	}
+		e.preventDefault();
+		dispatch(addTodo(input));
+		setInput("");
+	};
 
 	return (
-		<form onSubmit={addTodoHandler} className="space-x-3 mt-12 flex justify-between items-center">
+		<form
+			onSubmit={addTodoHandler}
+			className="space-x-3 mt-12 flex justify-between items-center"
+		>
 			<input
 				type="text"
 				className="bg-gray-800 rounded border border-gray-700 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-900 text-base outline-none text-gray-100 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out w-full"
