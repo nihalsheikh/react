30fccd79becdce9b4e26a0ddb3c0c46d782bbcc0
nihalsheikh <nihalsheikh585@gmail.com>
2025-08-30 08:53:20 +0530

    Data Conversion using Data Type classes

---
+++ 00_js_practice/004_datatype_conversion.js
@@ -0,0 +1,34 @@
+let score = "100";
+// console.log(typeof score);
+
+let num = Number(score);
+// console.log(num)
+// console.log(typeof num)
+
+let text = "";
+let text2 = "abc";
+
+let fname = String(num)
+// console.log(fname)
+// console.log(typeof fname)
+
+let bool1 = Boolean(text)
+let bool2 = Boolean(text2)
+// console.log(bool1);
+// console.log(bool2);
+
+// Boolean for string
+// "" => false
+// "abc" => true
+
+num = 1
+let numBool = Boolean(num)
+// console.log(numBool)
+
+num = 0
+let numBool2 = Boolean(num)
+console.log(numBool2)
+
+// Boolean for number
+//  0 => false
+// 1 or any number +ve or -ve or decimal => true
