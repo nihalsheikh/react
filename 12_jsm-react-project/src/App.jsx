13d60c3477fec467ad04372ad5500a6cd018c6b4
nihalsheikh <nihalsheikh585@gmail.com>
2025-09-03 22:27:04 +0530

    Components and CSS styling

--- 12_jsm-react-project/src/App.jsx
+++ 12_jsm-react-project/src/App.jsx
@@ -1,39 +1,45 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
 import "./App.css";
 
-function App() {
-	const [count, setCount] = useState(0);
-
+const Card = ({ title }) => {
 	return (
-		<>
-			<div>
-				<a href="https://vite.dev" target="_blank">
-					<img src={viteLogo} className="logo" alt="Vite logo" />
-				</a>
-				<a href="https://react.dev" target="_blank">
-					<img
-						src={reactLogo}
-						className="logo react"
-						alt="React logo"
-					/>
-				</a>
-			</div>
-			<h1>Vite + React</h1>
-			<div className="card">
-				<button onClick={() => setCount((count) => count + 1)}>
-					count is {count}
-				</button>
-				<p>
-					Edit <code>src/App.jsx</code> and save to test HMR
-				</p>
-			</div>
-			<p className="read-the-docs">
-				Click on the Vite and React logos to learn more
-			</p>
-		</>
+		<div
+			style={ {
+				border: "1px solid #4b5362",
+				padding: "20px",
+				margin: "10px",
+				backgroundColor: "#31363f",
+				borderRadius: "10px",
+				minHeight: "100px",
+			} }
+		>
+			<h2>{title}</h2>
+		</div>
 	);
-}
+};
+
+const App = () => {
+	return (
+		<div className="card-container">
+			<Card
+				title="John Wick"
+				rating={5}
+				isCool={true}
+				actors={[{ name: "Keanu Reeves" }]}
+			/>
+			<Card
+				title="Star Wars"
+				rating={5}
+				isCool={true}
+				actors={[{ name: "Samuel L. Jackson" }]}
+			/>
+			<Card
+				title="Bullet Train"
+				rating={5}
+				isCool={true}
+				actors={[{ name: "Bradd Pitt" }]}
+			/>
+		</div>
+	);
+};
 
 export default App;
