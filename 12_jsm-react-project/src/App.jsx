129bd8dc23752035c7ecd04349d962b11426969c
nihalsheikh <nihalsheikh585@gmail.com>
2025-09-08 11:02:01 +0530

    Added: Search Functionality and Debouncing

--- 12_jsm-react-project/src/App.jsx
+++ 12_jsm-react-project/src/App.jsx
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from "react";
 import Search from "./components/Search";
 import Spinner from "./components/Spinner";
 import MovieCard from "./components/MovieCard";
+import { useDebounce } from "react-use"
 
 const API_BASE_URL = "https://api.themoviedb.org/3";
 
@@ -20,13 +21,17 @@ const App = () => {
 	const [errorMessage, setErrorMessage] = useState("");
 	const [movieList, setMovieList] = useState([]);
 	const [isLoading, setIsLoading] = useState(false);
+	const [debouncedSearchTerm, setDebouncedSearchTerm] = useState("")
 
-	const fetchMovies = async () => {
+	// Debounce the search term to prevent too many API requests, by waiting for the user to stop typing for 500ms
+	useDebounce(() => setDebouncedSearchTerm(searchTerm), 500, [searchTerm])
+
+	const fetchMovies = async (query = "") => {
 		setIsLoading(true);
 		setErrorMessage("");
 
 		try {
-			const endpoint = `${API_BASE_URL}/discover/movie?sort_by=popularity.desc`;
+			const endpoint = query ? `${API_BASE_URL}/search/movie?query=${encodeURIComponent(query)}` : `${API_BASE_URL}/discover/movie?sort_by=popularity.desc`;
 
 			const response = await fetch(endpoint, API_OPTIONS);
 
@@ -54,8 +59,8 @@ const App = () => {
 	};
 
 	useEffect(() => {
-		fetchMovies();
-	}, []);
+		fetchMovies(debouncedSearchTerm); // using Debouncing to minimize API requests
+	}, [debouncedSearchTerm]);
 
 	return (
 		<main>
