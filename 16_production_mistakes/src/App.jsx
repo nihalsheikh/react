a0356301603c3b3789bac682d321039e38364aaf
nihal <nihalsheikh585@gmail.com>
2025-09-24 13:17:59 +0530

    Using && or Ternary to return jsx

--- 16_production_mistakes/src/App.jsx
+++ 16_production_mistakes/src/App.jsx
@@ -1,38 +1,27 @@
 import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
 import "./App.css";
 
 function App() {
-	const [count, setCount] = useState(0);
+	const [isLoggedIn, setIsLoggedIn] = useState(0);
 
 	return (
-		<>
+		<div style={{ padding: "2rem" }}>
+			<h1>Production Mistakes</h1>
+
+			<button onClick={() => setIsLoggedIn(!isLoggedIn)}>
+				Toggle Login
+			</button>
+
 			<div>
-				<a href="https://vite.dev" target="_blank">
-					<img src={viteLogo} className="logo" alt="Vite logo" />
-				</a>
-				<a href="https://react.dev" target="_blank">
-					<img
-						src={reactLogo}
-						className="logo react"
-						alt="React logo"
-					/>
-				</a>
+				<h3>&& operator</h3>
+				{isLoggedIn && <p>Chai code videos</p>}
 			</div>
-			<h1>Vite + React</h1>
-			<div className="card">
-				<button onClick={() => setCount((count) => count + 1)}>
-					count is {count}
-				</button>
-				<p>
-					Edit <code>src/App.jsx</code> and save to test HMR
-				</p>
+
+			<div>
+				<h3>Ternary operator</h3>
+				{isLoggedIn ? <p>Chai code videos</p> : <h2>Please login</h2>}
 			</div>
-			<p className="read-the-docs">
-				Click on the Vite and React logos to learn more
-			</p>
-		</>
+		</div>
 	);
 }
 
