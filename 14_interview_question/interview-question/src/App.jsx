d8438bb8ae91ebefed78101cd5abe637cccf005d
nihal <nihalsheikh585@gmail.com>
2025-09-24 10:50:39 +0530

    React interview question: component mount and render

---
+++ 14_interview_question/interview-question/src/App.jsx
@@ -0,0 +1,27 @@
+import { useState } from "react";
+import "./App.css";
+
+function App() {
+	const [value, setValue] = useState(1);
+
+	// const [multipliedValue, setMultipliedValue] = useState(1);
+
+	let multipliedValue = value * 5;
+
+	const multiplyNum = () => {
+		// setMultipliedValue(value * 5);
+		setValue(value + 1);
+	};
+
+	return (
+		<>
+			<h1>Main Value: {value} </h1>
+
+			<button onClick={multiplyNum}>Click to Multiply by 5</button>
+
+			<h2>Multiplied Value: {multipliedValue}</h2>
+		</>
+	);
+}
+
+export default App;
